4c75fccff710aa402233f519b49e91d87b46ee77
lex miranda <lexlopes9@hotmail.com>
2016-04-07 08:09:19 -0300

    Configurações do middleware

---
+++ Permissionamento/Permissionamento/Config/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace Permissionamento.Config
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(Data.Contexto.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<AppSignInManager>(AppSignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+                CookieName = "Libra",
+                CookiePath = "/"
+            });
+        }
+
+    }
+}
---
+++ Permissionamento/Permissionamento/Context/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Permissionamento.Context
+{
+    public class Contexto : IdentityDbContext
+    {
+        public Contexto()
+            : base(@"Data SourceFilter=(localdb)\mssqllocaldb ;Integrated Security=True; CatalogLocation=LibraPermissionamento; Connect Timeout=15; Encrypt=False; TrustServerCertificate=False")
+        {
+
+        }
+
+        public static Contexto Create()
+        {
+            return new Contexto();
+        }
+    }
+}
